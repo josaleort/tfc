5f857356469a87e6ca76df8abc8b623d3b586e19
josaleort <148063266+josaleort@users.noreply.github.com>
2025-05-25 23:32:31 +0200

    Create chatbot.php

---
+++ chatbot.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Chatbot</title>
+  <link rel="stylesheet" href="chatbot.css">
+</head>
+<body>
+  <div class="chatbot-contenedor">
+    <div class="chatbot-caja"></div>
+    <div class="chatbot-input">
+      <input type="text" id="pregunta" placeholder="Escribe tu pregunta">
+      <button id="boton-preguntar">Preguntar</button>
+    </div>
+  </div>
+  <script src="chatbot.js"></script>
+</body>
+</html>
+
